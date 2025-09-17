992c42c464848bf32cfafb18d9ace7ea8e23e6e9
kiwonl <61114855+kiwonl@users.noreply.github.com>
2025-09-17 12:20:00 +0000

    update terraform script

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,12 +6,13 @@ resource "google_project_service" "default" {
   project = data.google_project.project.project_id
   
   for_each = toset([
-    "cloudbuild.googleapis.com",        # Cloud Build API
-    "artifactregistry.googleapis.com",   # Artifact Registry API
-    "dns.googleapis.com",               # Google Cloud DNS API
-    "aiplatform.googleapis.com",        # Vertex AI API
-    "servicedirectory.googleapis.com",  # Service Directory API
-    "run.googleapis.com"                # Cloud Run API
+    "cloudbuild.googleapis.com",          # Cloud Build API
+    "artifactregistry.googleapis.com",    # Artifact Registry API
+    "dns.googleapis.com",                 # Google Cloud DNS API
+    "aiplatform.googleapis.com",          # Vertex AI API
+    "servicedirectory.googleapis.com",    # Service Directory API
+    "run.googleapis.com",                 # Cloud Run API
+    "cloudresourcemanager.googleapis.com" # Cloud Resource Manager API
   ])
 
   service            = each.value
